3c33f7524e2428a63ca51c5a0b8d0aaf18cc1dbe
George Vella <george.vella@betssongroup.com>
2018-01-02 16:55:26 +0100

    Refactored WebAppHost to remove IExpressRouteProvider Cleaned up useless logic from WebApplicationHost Removed unnecessary views from server-app to keep everything simple

--- Host/build.cmd
+++ Host/build.cmd
@@ -2,10 +2,21 @@
 
 REM cake build\main.cake -Verbosity=Diagnostic --nuget_source=https://sto-proget-01.bde.local/nuget/OBG-Tools --roslyn_nugetsource=https://sto-proget-01.bde.local/nuget/OBG-Tools
 
-for /F "delims=" %%G IN (build-order.txt) DO (
-    echo Building [%%G][loc: packages\node_modules\@jorge\%%G]
+@for /F "delims=" %%G IN (build-order.txt) DO @(
+    echo Building [%%G]
 
     pushd %~dp0\packages\node_modules\@jorge\%%G    
     tsc
+
+    if %ERRORLEVEL% NEQ 0 (
+        goto failure
+    )
     popd
 )
+
+
+:failure
+echo The Fuck, an error! Error: %ERRORLEVEL%
+
+:exit
+echo Built!
