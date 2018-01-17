2d23d5686024633a419c80b329424bba7f50692d
George Vella <george.vella@betssongroup.com>
2018-01-17 14:46:09 +0100

    Initial prep for individual packaging

--- Host/link.cmd
+++ Host/link.cmd
@@ -3,10 +3,10 @@
 REM cake build\main.cake -Verbosity=Diagnostic --nuget_source=https://sto-proget-01.bde.local/nuget/OBG-Tools --roslyn_nugetsource=https://sto-proget-01.bde.local/nuget/OBG-Tools
 
 @for /F "delims=" %%G IN (build-order.txt) DO @(
-    echo Building [%%G]
+    echo Linking [%%G]
 
-    pushd %~dp0\packages\node_modules\@jorge\%%G    
-    tsc --pretty
+    pushd %~dp0\packages\node_modules\@jorge\%%G
+    npm link
 
     if %ERRORLEVEL% NEQ 0 (
         goto failure
@@ -14,9 +14,10 @@ REM cake build\main.cake -Verbosity=Diagnostic --nuget_source=https://sto-proget
     popd
 )
 
+goto exit
 
 :failure
 echo The Fuck, an error! Error: %ERRORLEVEL%
 
 :exit
-echo Built!
+echo Packages linked into NPM globally!
